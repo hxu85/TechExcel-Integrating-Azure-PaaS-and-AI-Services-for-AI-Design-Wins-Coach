5e9e68b540b2b51b0ecadc9f2186a2a5e6305944
hongtaoxu-rwl <hongtaoxu@roguewavelabfy25.net>
2025-02-19 22:19:58 -0600

    Lab code change 1

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER [id-2qa3tgvoxmtaw] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [id-2qa3tgvoxmtaw];
+ALTER ROLE db_datawriter ADD MEMBER [id-2qa3tgvoxmtaw];
 
